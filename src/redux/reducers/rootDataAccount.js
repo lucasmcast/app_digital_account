639cbc316105b29bfd8c78a81345d3742ead3af9
LucasMCast <lucas.martins.c03@gmail.com>
2020-09-04 01:13:59 -0300

    ajustado tela de pagamneto e criado filtro por perido no extrato

--- src/redux/reducers/rootDataAccount.js
+++ src/redux/reducers/rootDataAccount.js
@@ -1,6 +1,6 @@
 import { ADD_LOGIN, ADD_DATA_ACCOUNT, UPDATE_BALANCE } from "../actionsTypes";
 import { conta } from "../../../res/data";
-import { Alert } from "react-native";
+
 
 const initialState = {
     dataAccount: {}
@@ -15,9 +15,7 @@ export default function (state = initialState, action) {
         case UPDATE_BALANCE: {
             const {typeTransaction, value} = action.payload
             let account = state.dataAccount
-            if(value ===0){
-                Alert.alert("Digite um valor valido")
-            }
+            
             if(typeTransaction === 1){  
                 account.saldoDisponivel -= value
                 return {...state, dataAccount: account}
